3382c28bccf333ddb8bd044b526c6cd10cdb3955
Alice Tseng <atseng@lcs-mba3235.lcs.on.ca>
2017-10-26 11:08:42 -0400

    not moving black circle

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    var dx : Int
+    
     // This function runs once
     override init() {
         
@@ -17,18 +19,36 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
-        
+        dx = 2
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        //clean up- drawa white rectangle over the entire canvas
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
+        
+        //change position
+        x += dx
+        
+        //make the circle bounce at the right edge
+        if x > 500{//start of block
+            dx = -2 //move left
+        } //end of block
+        
+        // make the circle bounce at the left edge
+        if x < 0 {
+            dx = 2 // move right
+        } //end of block
+        
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: 250, centreY: 250, width: 50, height: 50)
         
     }
     
+    
 }
+
