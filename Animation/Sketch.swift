9670786c235c12ace0b52b29a3a6cf9aa62d7113
Alice Tseng <atseng@lcs-mba3235.lcs.on.ca>
2017-10-28 11:18:25 -0400

    not moving 5 circles (problems with going opposite position)

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,7 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    
     // This function runs once
     override init() {
         
@@ -18,17 +19,35 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
+        // Change position to right
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
-        
+        // Draw ellipses going right
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: 30, width: 50, height: 50);
+        canvas.fillColor = Color.green
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50);
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(centreX: x, centreY: 470, width: 50, height: 50)
+      
+     x -= 1
+    //draw ellipses going left
+    canvas.fillColor = Color.blue
+    canvas.drawEllipse(centreX: x, centreY: 140, width: 50, height: 50);
+    canvas.fillColor = Color.orange
+    canvas.drawEllipse(centreX: x, centreY: 350, width: 50, height: 50)
+    
     }
     
-}
+
+  
+    }
+    
+
+
